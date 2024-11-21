a5d39e5402913e15158913f9b76ed4a89f4ebc92
Gabriel Carneiro <carneiro.development@gmail.com>
2024-11-21 01:29:31 -0400

    feat: add Question 1 solution

---
+++ src/questions/question1/Question1.tsx
@@ -0,0 +1,33 @@
+import React, { memo } from 'react'
+import { findDuplicated } from './helpers'
+
+const Question1 = memo(() => {
+
+  const [duplicated, setDuplicated] = React.useState<unknown[]>([])
+  const [input, setInput] = React.useState<string>('')
+
+  const onClick = () => {
+    try {
+      const duplicates = findDuplicated(JSON.parse(input))
+      setDuplicated(duplicates)
+    } catch (error) {
+      console.error('Invalid input:', error)
+      setDuplicated([])
+    }
+  }
+
+  return (
+    <section>
+      <h2>Question 1</h2>
+      <p>Write a javascript function that finds the duplicate items in any given array.</p>
+
+      <div>
+        <input type="text" name='input' className='input' value={input} onChange={(event) => setInput(event.target.value)} />
+        <button onClick={onClick}>Find duplicated!</button>
+        <p>duplicated values: {JSON.stringify(duplicated)}</p>
+      </div>
+    </section>
+  )
+})
+
+export default Question1
---
+++ src/questions/question1/helpers.ts
@@ -0,0 +1,29 @@
+/**
+ * 1) Write a javascript function that finds the duplicate items in any given array
+ */
+
+export function findDuplicated(input: unknown[]) {
+    const duplicates = [];
+    const checked = new Set();
+    const duplicated = new Set();
+
+    for (const item of input) {
+        const itemString = JSON.stringify(item);
+
+        if (checked.has(itemString)) {
+            if (!duplicated.has(itemString)) {
+                duplicates.push(item);
+                duplicated.add(itemString);
+            }
+        } else {
+            checked.add(itemString);
+        }
+    }
+
+    return duplicates;
+}
+
+
+
+
+
